ebf0065a6bf748cbbb49f32c50ae8e36e7205e55
Tahaberk Soysal <berksysl.g@gmail.com>
2025-12-10 16:56:24 +0300

    apply design changes at pricing section

--- src/components/sections/landing/Pricing.tsx
+++ src/components/sections/landing/Pricing.tsx
@@ -1,6 +1,7 @@
 import Badge from "@/components/ui/Badge";
 import Image from "next/image";
 import Link from "next/link";
+import Button from "@/components/ui/Button";
 
 export default function Pricing() {
   return (
@@ -12,27 +13,29 @@ export default function Pricing() {
         <Badge className="tablet:mb-6 desktop:mb-8 mx-0 mb-5" />
         {/* Text Content */}
         <div className="mb-10 max-w-[616px]">
-          <h2 className="mb-6 text-5xl leading-16 font-medium">
+          <h2 className="tablet:text-[56px] tablet:leading-14 desktop:text-5xl desktop:leading-[68px] mb-6 text-[28px] leading-10 font-medium">
             Fiyatlandırma
           </h2>
-          <p className="text-secondary-color text-2xl leading-8 font-medium">
+          <p className="text-secondary-color tablet:text-2xl desktop:text-[28px] tablet:leading-[34px] desktop:leading-10 text-lg leading-[26px] font-medium">
             Lorem ipsum dolor sit amet consectetur. Leo aliquam elit adipiscing
             mi massa nibh sit. Diam praesent elit a lorem.
           </p>
         </div>
 
         {/* Card */}
-        <div className="border-effect tablet:flex-row tablet:gap-16 mx-auto flex max-w-[1264px] flex-col gap-16 rounded-[20px] bg-[#CEEAB0] p-10 before:rounded-[20px] after:rounded-[20px]">
+        <div className="border-primary-color tablet:flex-row tablet:gap-16 tablet:justify-evenly mx-auto flex max-w-[1264px] flex-col justify-between rounded-[20px] border border-r-4 border-b-4 bg-[#CEEAB0] p-8">
           <div className="desktop:max-w-[560px] max-w-[392px]">
-            <h3 className="mb-10 text-4xl leading-[50px] font-bold">
+            <h3 className="tablet:text-[32px] tablet:leading-[46px] desktop:text-[40px] desktop:leading-14 mb-6 text-2xl leading-[34px] font-bold">
               Online rezervasyon takibi
             </h3>
-            <p className="text-secondary-color mb-10 text-xl leading-[28px] font-medium">
+            <p className="text-secondary-color tablet:text-xl desktop:text-2xl tablet:leading-7 desktop:leading-[34px] mb-10 text-base leading-6 font-medium">
               Rezervasyon yönetiminden gelir takibine kadar tüm özellikleri tek
               pakette kullanın.
             </p>
             <div className="flex items-center gap-3">
-              <span className="text-xl font-medium line-through">₺5000</span>
+              <span className="tablet:text-[32px] desktop:text-[40px] text-2xl font-medium line-through">
+                ₺5000
+              </span>
               <h4 className="text-4xl font-bold">
                 ₺2000<span className="text-xl font-medium">/Aylık</span>
               </h4>
@@ -40,75 +43,75 @@ export default function Pricing() {
             <span className="text-secondary-color mb-6 block text-base font-medium">
               12 ay boyunca sabit fiyat
             </span>
-            <Link
-              href="/login"
-              className="btn-primary border-effect flex max-w-max gap-2.5 rounded-[8px] px-4.5 py-3 text-base font-medium before:rounded-[8px] after:rounded-[8px]"
-            >
-              <Image
-                src="/images/linkarrow.svg"
-                alt="link icon"
-                width={15}
-                height={15}
-              />
-              14 Gün Ücretsiz Deneyin
+            <Link href="/login">
+              <Button className="tablet:w-auto mb-10 w-[284px] py-3">
+                14 Gün Ücretsiz Deneyin
+                <Image
+                  src="/images/linkarrow.svg"
+                  alt="link icon"
+                  width={15}
+                  height={15}
+                  className="ml-2"
+                />
+              </Button>
             </Link>
           </div>
           <div>
-            <h3 className="mb-10 text-2xl font-medium">
-              Online rezervasyon takibi
+            <h3 className="tablet:text-[28px] desktop:text-[34px] mb-4 text-xl font-bold">
+              Özellikler
             </h3>
-            <div className="flex flex-col gap-4">
-              <p className="flex gap-3 text-xl font-medium">
+            <div className="flex flex-col gap-3">
+              <p className="tablet:text-xl desktop:text-2xl flex gap-3 text-base font-medium">
                 <Image
-                  src="/images/ballIcon.svg"
-                  alt="football icon"
-                  width={24}
-                  height={24}
+                  src="/images/thickIcon.svg"
+                  alt="thick"
+                  width={20}
+                  height={20}
                 />
                 Sınırsız online rezervasyon
               </p>
-              <p className="flex gap-3 text-xl font-medium">
+              <p className="tablet:text-xl desktop:text-2xl flex gap-3 text-base font-medium">
                 <Image
-                  src="/images/ballIcon.svg"
-                  alt="football icon"
-                  width={24}
-                  height={24}
+                  src="/images/thickIcon.svg"
+                  alt="thick"
+                  width={20}
+                  height={20}
                 />
                 Kolay saha takvimi yönetimi
               </p>
-              <p className="flex gap-3 text-xl font-medium">
+              <p className="tablet:text-xl desktop:text-2xl flex gap-3 text-base font-medium">
                 <Image
-                  src="/images/ballIcon.svg"
-                  alt="football icon"
-                  width={24}
-                  height={24}
+                  src="/images/thickIcon.svg"
+                  alt="thick"
+                  width={20}
+                  height={20}
                 />
                 Otomatik SMS ve e-posta bildirimleri
               </p>
-              <p className="flex gap-3 text-xl font-medium">
+              <p className="tablet:text-xl desktop:text-2xl flex gap-3 text-base font-medium">
                 <Image
-                  src="/images/ballIcon.svg"
-                  alt="football icon"
-                  width={24}
-                  height={24}
+                  src="/images/thickIcon.svg"
+                  alt="thick"
+                  width={20}
+                  height={20}
                 />
                 Gelir ve doluluk analizi
               </p>
-              <p className="flex gap-3 text-xl font-medium">
+              <p className="tablet:text-xl desktop:text-2xl flex gap-3 text-base font-medium">
                 <Image
-                  src="/images/ballIcon.svg"
-                  alt="football icon"
-                  width={24}
-                  height={24}
+                  src="/images/thickIcon.svg"
+                  alt="thick"
+                  width={20}
+                  height={20}
                 />
                 Tüm cihazlarda yönetim paneli
               </p>
-              <p className="flex gap-3 text-xl font-medium">
+              <p className="tablet:text-xl desktop:text-2xl flex gap-3 text-base font-medium">
                 <Image
-                  src="/images/ballIcon.svg"
-                  alt="football icon"
-                  width={24}
-                  height={24}
+                  src="/images/thickIcon.svg"
+                  alt="thick"
+                  width={20}
+                  height={20}
                 />
                 Bulut tabanlı güvenli sistem
               </p>
