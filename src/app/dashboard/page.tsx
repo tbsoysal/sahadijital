d9c0da72c571d6bc6c0ab2363901e645bb08e7bf
Tahaberk SOYSAL <berksysl.g@gmail.com>
2026-02-26 02:59:17 +0300

    edit proxy to authorization for routes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,32 +1,6 @@
-import { createClient } from "@/lib/supabase/server";
-import { redirect } from "next/navigation";
 import { WeeklyCalendar } from "@/components/dashboard/calendar/WeeklyCalendar";
 
-export default async function Dashboard() {
-  const supabase = await createClient();
-
-  // 1. Verify the user is authenticated
-  const {
-    data: { user },
-    error: authError,
-  } = await supabase.auth.getUser();
-
-  if (authError || !user) {
-    console.log(authError, user);
-    redirect("/login");
-  }
-
-  // 2. Fetch first_name and last_name from the 'profiles' table
-  const { data: profile, error: profileError } = await supabase
-    .from("profiles")
-    .select("first_name, last_name")
-    .eq("id", user.id) // The 'id' in profiles should match the 'id' from auth.users
-    .single();
-
-  if (profileError) {
-    // Handle cases where the profile might not exist yet
-    console.error("Error loading profile:", profileError.message);
-  }
+export default function Dashboard() {
 
   return <WeeklyCalendar />;
 }
--- src/lib/supabase/proxy.ts
+++ src/lib/supabase/proxy.ts
@@ -43,8 +43,7 @@ export async function updateSession(request: NextRequest) {
 
   if (
     !user &&
-    !request.nextUrl.pathname.startsWith("/login") &&
-    !request.nextUrl.pathname.startsWith("/auth")
+    request.nextUrl.pathname.startsWith("/dashboard")
   ) {
     // no user, potentially respond by redirecting the user to the login page
     const url = request.nextUrl.clone();
